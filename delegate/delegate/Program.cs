e0d3c4eaa3ad8bcf4e191b97d521b26e0bd4509d
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2024-12-04 10:43:57 +0545

    delegate

---
+++ delegate/delegate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Define a delegate
+//helps to call a method indirectly
+// That means, a delegate holds the address of a method which can be called using that delegate.
+public delegate void Operation(string message); //return type same hunu parxa delegate rw point gareko function ko
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        Operation operation = new Operation(MyFunc); //always takes function while using delegate
+        Operation operation2 = new Operation(MyFunc2);
+        operation("Hello"); //Invocation
+        operation2("Hi");
+    }
+    // Method that takes a delegate as parameter
+    public static void MyFunc(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static void MyFunc2(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
